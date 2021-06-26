db49a7d8e738a6ac033e66b9fe802f8c326070b7
mrozowski <mrozowskion@gmail.com>
2021-06-26 19:57:26 +0200

    init bridge

---
+++ Bridge/src/test/java/com/mrozowski/bridge/BridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrozowski.bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BridgeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
